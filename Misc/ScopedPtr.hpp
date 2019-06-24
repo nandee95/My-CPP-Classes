a74777b0688bf170bf3ef0041b424db91149fdce
Nándor Szalma <szalma.nandor@gmail.com>
2019-06-24 22:51:35 +0200

    ScopedPtr documentation

--- Misc/ScopedPtr.hpp
+++ Misc/ScopedPtr.hpp
@@ -1,44 +1,77 @@
+/*
+Licensed under the MIT license
+Copyright (c) 2019 Nandor Szalma
+Github: https://github.com/nandee95/My_CPP_Classes
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 #pragma once
 
-template <typename T>
+/// <summary> Scoped pointer implementation </summary>
+/// <template name="T"> Type of the pointer </template>
+/// <template name="A"> True if the pointer is array </template>
+template <typename T, bool A = false>
 class ScopedPtr
 {
 public:
-	T* ptr;
+	T* ptr = nullptr; //Stored pointer
+	
+	/// <summary> Initializes the stored pointer to the given pointer </summary>
 	ScopedPtr(T* ptr) : ptr(ptr)
-	{
+	{ }
 
-	}
-
-	ScopedPtr() : ptr(nullptr)
-	{
-
-	}
+	/// <summary> Casting to the type of the stored pointer </summary>
+	/// <returns> Stored pointer </returns>
 	inline operator T* () const noexcept
 	{
 		return ptr;
 	}
 
+	/// <summary> Sets the stored pointer </summary>
 	void operator = (T* ptr) noexcept
 	{
 		if (ptr != nullptr) delete this->ptr;
 		this->ptr = ptr;
 	}
+
+	/// <summary> Returns the object the stored pointer points to </summary>
+	/// <returns> Pointed object </returns>
 	inline T& operator *() const noexcept
 	{
 		return *ptr;
 	}
 
+	/// <summary> References the functions of the stored pointer </summary>
+	/// <returns> True on success </returns>
 	inline T* operator ->() const noexcept
 	{
 		return ptr;
 	}
 
+	/// <summary> Returns true if the stored pointer is valid </summary>
+	/// <returns> State of the pointer </returns>
 	inline operator bool() const noexcept
 	{
 		return ptr == nullptr;
 	}
 
+	/// <summary> Frees the memory if necessary </summary>
 	~ScopedPtr()
 	{
 		if (ptr != nullptr) delete ptr;
--- README.md
+++ README.md
@@ -7,16 +7,30 @@ I started documenting and licensing the classes.
 Detailed documentation of the listed classes can be found in the source code!
 
 <h3>WinAPI / ProcessMemory.hpp</h3>
-This class capable of reading and writing values through multi level pointers from any process's memory. The process can be opened by window title or executable name.
+This class capable of reading and writing values through multi level pointers from any process's memory. The process can be opened by window title or executable name. <i>(The order of pointer offsets are the reverse of what CheatEngine shows)</i>
 
 ```cpp
 ProcessMemory pm;
 pm.OpenByExecutableName("process.exe");
+//or pm.OpenByWindowTitle("My window");
 int32_t value;
 while (pm.IsValid())
 {
-	pm.ReadMemoryValue(value, pm.GetModuleAddr("process.exe") + 0x00ABCDEF, { 0x123, 0x234, 0x345, 0x456});
-	pm.WriteMemoryValue<int32_t>(1111, pm.GetModuleAddr("process.exe") + 0x00123456);
+	pm.ReadMemoryValue(value, pm.GetModuleAddr("process.exe") + 0x00ABCDEF, { 0x123, 0x234, 0x345, 0x456}); //Read multi level pointer
+	pm.WriteMemoryValue<int32_t>(1111, pm.GetModuleAddr("example.dll") + 0x00123456); //Read value
 }
 pm.Close();
+```
+
+<h3>Misc / ScopedPtr.hpp</h3>
+Scoped pointer implementation. The class' destructior takes care of the release of the memory. It's used to create memory leak free code.
+
+```cpp
+ScopedPtr<int> sptr(new int);
+*sptr = 44;
+std::cout << *sptr << std::endl;
+
+ScopedPtr<int, true> sptrarray(new int[10000]);
+sptrarray[100] = 44;
+std::cout << sptrarray[100] << std::endl;
 ```
